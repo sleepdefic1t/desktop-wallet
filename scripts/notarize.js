9f77c7d0fc45300e130579888641ce01a80ba5f4
Lucio Rubens <4539235+luciorubeens@users.noreply.github.com>
2019-12-05 09:28:23 -0300

    build: notarize app

---
+++ scripts/notarize.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-return-await */
+const { notarize } = require('electron-notarize')
+
+exports.default = async function notarizing (context) {
+  const { electronPlatformName, appOutDir } = context
+  if (electronPlatformName !== 'darwin') {
+    return
+  }
+
+  const appName = context.packager.appInfo.productFilename
+
+  return await notarize({
+    appBundleId: 'io.ark.desktop-wallet',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASS
+  })
+}

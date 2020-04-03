21a05df9c161f7d022dd76f888c0f77f8b70249e
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2020-04-03 10:21:25 -0700

    rm sign ecdsa tx test

--- __tests__/unit/services/transaction.spec.js
+++ __tests__/unit/services/transaction.spec.js
@@ -842,7 +842,7 @@ describe('Services > Transaction', () => {
     })
   })
 
-  describe('ledgerSign', () => {
+  describe('ledgerSignWithSchnorr', () => {
     const vmMock = {
       $store: {
         // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -873,31 +873,6 @@ describe('Services > Transaction', () => {
     afterEach(() => {
       spyDispatch.mockRestore()
       spyTranslate.mockRestore()
-    })
-
-    it('should ecdsa sign the transaction', async () => {
-      transactionObject.sign(senderPassphrase)
-      const transactionJson = transactionObject.getStruct()
-
-      const bytes = TransactionService.getBytes(transactionJson)
-      const id = TransactionService.getId(transactionJson)
-      const signature = transactionObject.data.signature
-
-      spyDispatch.mockImplementation((key) => {
-        if (key === 'ledger/signTransaction') {
-          return signature
-        }
-      })
-
-      const spyGetBytes = jest.spyOn(TransactionService, 'getBytes')
-
-      const transaction = await TransactionService.ledgerSign(wallet, transactionObject, vmMock)
-
-      expect(transaction.id).toEqual(id)
-      expect(spyGetBytes).toHaveBeenCalledWith(transactionJson)
-      expect(spyGetBytes).toHaveReturnedWith(bytes)
-
-      spyGetBytes.mockRestore()
     })
 
     it('should schnorr sign the transaction', async () => {
@@ -935,7 +910,7 @@ describe('Services > Transaction', () => {
       const signature = transactionObject.data.signature
 
       spyDispatch.mockImplementation((key) => {
-        if (key === 'ledger/signTransaction') {
+        if (key === 'ledger/signTransactionWithSchnorr') {
           return signature
         }
       })
@@ -968,7 +943,7 @@ describe('Services > Transaction', () => {
       const signature = transactionObject.data.signature
 
       spyDispatch.mockImplementation((key) => {
-        if (key === 'ledger/signTransaction') {
+        if (key === 'ledger/signTransactionWithSchnorr') {
           return signature
         }
       })
